ce18b5871d6c68c7f50f29db6236ccccc8ad61ea
IlliterateElf <illiterate.elf@outlook.com>
2022-06-21 17:59:35 -0700

    finished code and added comments
    The code is exactly as the site shows
    
    The comments are my own as I analyze exactly what every part of the code is doing

--- main.py
+++ main.py
@@ -11,20 +11,125 @@ from pandas.plotting import scatter_matrix
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
+# reads data to a dataset and adds the column cleaned_resume
 resumeDataSet = pd.read_csv('UpdatedResumeDataSet.csv' ,encoding='utf-8')
 resumeDataSet['cleaned_resume'] = ''
 resumeDataSet.head()
 
+# displays each mentioned category and its use count
+print ("Displaying the distinct categories of resume -")
+print (resumeDataSet['Category'].unique())
+print ("Displaying the distinct categories of resume and the number of records belonging to each category -")
+print (resumeDataSet['Category'].value_counts())
+
+# makes a Seaborn countplot of the use count of the categories and displays them on a 90 turned matplotlib window
+import seaborn as sns
+plt.figure(figsize=(15,15))
+plt.xticks(rotation=90)
+sns.countplot(y="Category", data=resumeDataSet)
+plt.show()
+
+# makes values for use count and categories
 from matplotlib.gridspec import GridSpec
 targetCounts = resumeDataSet['Category'].value_counts()
 targetLabels = resumeDataSet['Category'].unique()
 
+# new figure
 plt.figure(1, figsize=(25,25))
 the_grid = GridSpec(2, 2)
 
+# gets a set with even distro of colors for the graph and lays ground for the 
 cmap = plt.get_cmap('coolwarm')
 colors = [cmap(i) for i in np.linspace(0, 1, 3)]
 plt.subplot(the_grid[0,1], aspect=1, title='CATEGORY DISTRIBUTION')
 
+# makes the pie chart targetCounts for the data in the wedges formatted to be a percent with one trailing decimal and their appropriate labels. Also assigns colors from before and gives graph a shadow.
 source_pie = plt.pie(targetCounts, labels=targetLabels, autopct='%1.1f%%', shadow=True, colors=colors)
 plt.show()
+
+import re
+
+def cleanResume(resumeText):
+    resumeText = re.sub('http\S+\s*', ' ', resumeText) # remove URLs
+    resumeText = re.sub('RT|cc', ' ', resumeText) # remove RT and cc
+    resumeText = re.sub('#\S+', '', resumeText) # remove hashtags
+    resumeText = re.sub('@\S+', ' ', resumeText)
+    resumeText = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', resumeText)  # remove punctuations
+    resumeText = re.sub(r'[^\x00-\x7f]',r' ', resumeText) 
+    resumeText = re.sub('\s+', ' ', resumeText)  # remove extra whitespace
+    return resumeText
+
+# cleans the resume up and adds the cleaned values to the cleaned_resume column
+resumeDataSet['cleaned_resume'] = resumeDataSet.Resume.apply(lambda x: cleanResume(x))
+
+import nltk
+from nltk.corpus import stopwords
+import string
+from wordcloud import WordCloud
+# sets stop words not needed, makes totalwords and cleanedSentences var, and assigns Sentences with the values in the Resume column of the dataset
+oneSetOfWords = set(stopwords.words('english')+['``',"''"])
+totalWords = []
+Sentences = resumeDataSet['Resume'].values
+cleanedSentences = ""
+
+# for the range, clean every sentence and add the text to cleanedSentences
+# then tokenize the words to split up the words and make sure it's not a stop word
+# if not a stop word, add it to the totalWords
+for i in range(0,160):
+    cleanedText = cleanResume(Sentences[i])
+    cleanedSentences += cleanedText
+    requiredWords = nltk.word_tokenize(cleanedText)
+    for word in requiredWords:
+        if word not in oneSetOfWords and word not in string.punctuation:
+            totalWords.append(word)
+
+# make a frequency distribution and print the 50 most common
+wordfreqdist = nltk.FreqDist(totalWords)
+mostcommon = wordfreqdist.most_common(50)
+print(mostcommon)
+
+# make a wordcloud from cleanedSentences, make a new matplot figure and show it as a 2D figure with no axis
+wc = WordCloud().generate(cleanedSentences)
+plt.figure(figsize=(15,15))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+from sklearn.preprocessing import LabelEncoder
+
+var_mod = ['Category']
+le = LabelEncoder()
+for i in var_mod:
+    resumeDataSet[i] = le.fit_transform(resumeDataSet[i])
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import hstack
+# logically defines text as the info input and target as the guess output
+requiredText = resumeDataSet['cleaned_resume'].values
+requiredTarget = resumeDataSet['Category'].values
+
+# learns the required text of every entry by frequency
+word_vectorizer = TfidfVectorizer(
+    sublinear_tf=True,
+    stop_words='english',
+    max_features=1500)
+word_vectorizer.fit(requiredText)
+WordFeatures = word_vectorizer.transform(requiredText)
+
+print ("Feature completed .....")
+
+# splits train test with a test size of 20% and returns num of data in the train and test lists
+X_train,X_test,y_train,y_test = train_test_split(WordFeatures,requiredTarget,random_state=0, test_size=0.2)
+print(X_train.shape)
+print(X_test.shape)
+
+# sets up prediction method and prints the accurracy score formatted up to two decimals
+clf = OneVsRestClassifier(KNeighborsClassifier())
+clf.fit(X_train, y_train)
+prediction = clf.predict(X_test)
+print('Accuracy of KNeighbors Classifier on training set: {:.2f}'.format(clf.score(X_train, y_train)))
+print('Accuracy of KNeighbors Classifier on test set: {:.2f}'.format(clf.score(X_test, y_test)))
+
+# final report
+print("\n Classification report for classifier %s:\n%s\n" % (clf, metrics.classification_report(y_test, prediction)))
